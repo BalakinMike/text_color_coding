0f9251ee09ae513c263527b99b6a568f5987d771
BalakinMike <mikb2220@gmail.com>
2024-01-06 20:26:02 +0300

    GIU searcing json file

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Project "Text_color" contains app to translate text into color code.
 <ol>
     <li>Yes! It's work! Currently, app can translates texts (only English and Russian) into color squares. However, I not have interest ideas about coding algorithms. I have created primitive dictionary with random pairs <b>"letter - color"</b>.</li>
-    <li>Work continue...</li>
+    <li>Realised  searching and choosing in GUI json file with coding table.</li>
 </ol>  
 
--- color_coding.py
+++ color_coding.py
@@ -4,19 +4,23 @@ import os
 
 NAME_SIZE = 23
 
-def name(name):
-   
-    return sg.Text(name + ':', size=(NAME_SIZE,1), justification='l', pad=(0,0), font='Courier 20')
-
-with open('color_code_dict.json') as f:
-    color_code_dict = json.load(f)
+def json_to_dict(name):
+   '''Converting json table to coding dictionary'''
+   with open(name) as f:
+        color_code_dict = json.load(f)
+   return color_code_dict
 
 
 rightclick=['&Edit', ['C&ut', '&Copy','&Paste', '&Undo']]
 menu_def = [['&File', ['&New', '&Open', '&Save', 'E&xit', ]], ['Edit', ['Cut', 'Copy', 'Paste', 'Undo'], ],  ['Help', 'About...'], ]
 
 # ----- Full layout -----
-layout = [[sg.Menu(menu_def)],
+layout = [[
+            sg.Text("Папка с исходными файлами"),
+            sg.In(size=(25, 1), enable_events=True, key="-FILE-"),
+            sg.FileBrowse('Обзор'),
+           ],
+          [sg.Menu(menu_def)],
           [sg.Text("Text, which going to translate in color code:")],
           [sg.Multiline("", key='-IN-', expand_x=True, expand_y=True)],
           [sg.Graph(canvas_size=(400,100),graph_bottom_left=(0,0), 
@@ -36,7 +40,10 @@ while True:
     
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    if event == '-TRANSFORM-':
+    if event == '-FILE-': #Searching json file with coding table
+        pth = window['-FILE-'].get()
+        color_code_dict = json_to_dict(pth)
+    if event == '-TRANSFORM-': #Transforming text to color line
         txt = window['-IN-'].get()
         
         for i in range(len(txt)):
@@ -45,7 +52,7 @@ while True:
                 rectangle = graph.draw_rectangle((10*i,100), (10*i+10,90), line_color=f'{color_code_dict[hex_code_sym]}')
             except KeyError:
                 window['-OUT-'].update(f'Key {hex_code_sym} not found in color_code_dict')
-
+            
             graph.TKCanvas.itemconfig(rectangle, fill= f'{color_code_dict[hex_code_sym]}')
         print(event, values)
 window.close()
